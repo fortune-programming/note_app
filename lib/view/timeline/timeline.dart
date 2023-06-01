0718aa45ec68ef47017089e3254f34ba402849ed
ayana <522346@m.mie-u.ac.jp>
2023-06-01 18:07:49 +0900

    I recreate 'timeline.dart',but this is not perfect

--- lib/view/timeline/timeline.dart
+++ lib/view/timeline/timeline.dart
@@ -1,37 +1,93 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+class SubmittedData {
+  final String name;
+  final String email;
+
+  SubmittedData({required this.name, required this.email});
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SubmittedDataPage extends StatelessWidget {
+  final SubmittedData submittedData;
+
+  SubmittedDataPage({required this.submittedData});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('タブ画面移動サンプル'),
-            bottom: const TabBar(tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.cloud_outlined)  
-                  ),
-              Tab(icon: Icon(Icons.beach_access_sharp)),
-              Tab(icon: Icon(Icons.brightness_5_sharp)),
-            ]),
-          ),
-          body: const TabBarView(
-            children: <Widget>[
-              Center(child: Text('くもり', style: TextStyle(fontSize: 50))),
-              Center(child: Text('雨', style: TextStyle(fontSize: 50))),
-              Center(child: Text('晴れ', style: TextStyle(fontSize: 50))),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Submitted Data'),
+        backgroundColor: Color(0xFFE2D4BA),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Name: ${submittedData.name}'),
+            Text('Email: ${submittedData.email}'),
+            // Display more properties as needed
+          ],
         ),
       ),
     );
   }
+}
+
+class SubmitDataForm extends StatefulWidget {
+  @override
+  _SubmitDataFormState createState() => _SubmitDataFormState();
+}
+
+class _SubmitDataFormState extends State<SubmitDataForm> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Submit Data'),
+        backgroundColor: Color(0xFFE2D4BA),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                String name = nameController.text;
+                String email = emailController.text;
+                SubmittedData submittedData =
+                    SubmittedData(name: name, email: email);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        SubmittedDataPage(submittedData: submittedData),
+                  ),
+                );
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Data Submission',
+    home: SubmitDataForm(),
+  ));
 }
