078c666330fabe26d1339e6dc556284ce8ca061b
Takase2205 <137969597+Takase2205@users.noreply.github.com>
2023-08-09 14:24:45 +0900

    Create initial_register.dart

---
+++ lib/view/initial_register/initial_register.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class InitialRegister extends StatefulWidget {
+
+  @override
+  _InitialRegisterState createState() => _InitialRegisterState();
+}
+
+class _InitialRegisterState extends State<InitialRegister> {
+    TextEditingController emailController = TextEditingController();
+    
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:SafeArea(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(height: 50,),
+              Text("ノート共有アプリへようこそ！",style: TextStyle(fontSize: 24, fontWeight:FontWeight.bold),),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical:20.0),
+                child: Container(
+                  width: 300,
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      hintText: 'メールアドレス',
+                      suffixIcon: Padding(padding: const
+                      EdgeInsets.all(12.0),
+                        child: Text('@m.mie-u.ac.jp',style: TextStyle(color: Colors.black),),
+                      ),
+                     ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 70,),
+               ElevatedButton(onPressed: () {}, 
+               child: Text('認証コードを送信')),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
