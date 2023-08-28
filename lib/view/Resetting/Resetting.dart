f56453223153e65ddbf6cac8899cb8b90da671f8
423839hayato <423839@m.mie-u.ac.jp>
2023-08-28 21:08:45 +0900

    change a page name:Resetting.dart

--- lib/view/Resetting/Resetting.dart
+++ lib/view/Resetting/Resetting.dart
@@ -13,54 +13,46 @@ class _ResettingPageState extends State<ResettingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          child: Column(
-            children: [
-              SizedBox(height: 58,),
-              Text('再設定ページ', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                child: Container(
-                  width: 300,
-                  child: TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      hintText: 'メールアドレス'
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width:300,
+        body: SafeArea(
+      child: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 58,
+            ),
+            Text(
+              '再設定ページ',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Container(
+                width: 300,
                 child: TextField(
-                  controller: passController,
-                  decoration: InputDecoration(
-                    hintText: 'パスワード'
-                  ),
+                  controller: emailController,
+                  decoration: InputDecoration(hintText: 'パスワード'),
                 ),
               ),
-              SizedBox(height: 10),
-              RichText(
-                text: TextSpan(
-                  style: TextStyle(color: Colors.black),
-                  children: [
-                    TextSpan(text: '新しいパスワードを設定してください。'),
-                  ]
-                ),
+            ),
+            Container(
+              width: 300,
+              child: TextField(
+                controller: passController,
+                decoration: InputDecoration(hintText: 'パスワード（確認用）'),
               ),
-              SizedBox(height: 70),
-              ElevatedButton(
-                onPressed: () {
-
-                },
-                child: Text('完了')
-              )
-            ],
-          ),
+            ),
+            SizedBox(height: 10),
+            RichText(
+              text: TextSpan(style: TextStyle(color: Colors.black), children: [
+                TextSpan(text: '新しいパスワードを設定してください。'),
+              ]),
+            ),
+            SizedBox(height: 70),
+            ElevatedButton(onPressed: () {}, child: Text('完了'))
+          ],
         ),
-      )
-    );
+      ),
+    ));
   }
 }
