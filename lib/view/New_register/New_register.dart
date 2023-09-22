525cf9935dc577b9e4307066349aa5f50e2045e3
423839hayato <423839@m.mie-u.ac.jp>
2023-09-22 21:15:46 +0900

    change screen and New_register

--- lib/view/New_register/New_register.dart
+++ lib/view/New_register/New_register.dart
@@ -11,35 +11,38 @@ class _New_registerState extends State<New_register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 58,
-          ),
-          Text(
-            '新規登録ページ',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20.0),
-            child: Container(
-              width: 300,
-              child: TextField(
-                controller: passController,
-                decoration: InputDecoration(hintText: 'パスワード'),
+        body: Container(
+      alignment: Alignment.center,
+      child: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 58,
+            ),
+            Text(
+              '新規登録ページ',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Container(
+                width: 300,
+                child: TextField(
+                  controller: passController,
+                  decoration: InputDecoration(hintText: 'パスワード'),
+                ),
               ),
             ),
-          ),
-          Container(
-            width: 300,
-            child: TextField(
-              decoration: InputDecoration(hintText: 'パスワード（確認用）'),
+            Container(
+              width: 300,
+              child: TextField(
+                decoration: InputDecoration(hintText: 'パスワード（確認用）'),
+              ),
             ),
-          ),
-          SizedBox(height: 70),
-          ElevatedButton(onPressed: () {}, child: Text('完了'))
-        ],
+            SizedBox(height: 70),
+            ElevatedButton(onPressed: () {}, child: Text('完了'))
+          ],
+        ),
       ),
     ));
   }
--- lib/view/screen.dart
+++ lib/view/screen.dart
@@ -3,7 +3,7 @@ import 'dart:developer';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune/main.dart';
-import 'package:flutter_fortune/view/mypage/mypage_edit.dart';
+import 'package:flutter_fortune/view/Mypage_edit/Mypage_edit.dart';
 import 'package:flutter_fortune/view/note/post_note_meta.dart';
 
 import 'mypage/mypage.dart';
@@ -15,42 +15,42 @@ class Screen extends StatefulWidget {
   @override
   State<Screen> createState() => _ScreenState();
 }
+
 class _ScreenState extends State<Screen> {
-  int selectedIndex =0;
-  List<Widget>pageList = [Timeline(), mypage()];
+  int selectedIndex = 0;
+  List<Widget> pageList = [Timeline(), mypage()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:pageList[selectedIndex],
+      body: pageList[selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home_outlined,
-              color: Color.fromRGBO(226, 212, 186, 100),),
-            label: ''
-          ),
+              icon: Icon(
+                Icons.home_outlined,
+                color: Color.fromRGBO(226, 212, 186, 100),
+              ),
+              label: ''),
           BottomNavigationBarItem(
-            icon: Icon(
-              Icons.perm_identity_outlined,
-              color: Color.fromRGBO(226, 212, 186, 100),),
-            label: ''
-          )
+              icon: Icon(
+                Icons.perm_identity_outlined,
+                color: Color.fromRGBO(226, 212, 186, 100),
+              ),
+              label: '')
         ],
-        currentIndex: selectedIndex,  
-        onTap: (index){
-          setState((){
-            selectedIndex=index;
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
           });
         },
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: () {
-          },
-          child: Icon(Icons.chat_bubble_outline),
-          backgroundColor: Color.fromRGBO(226, 212, 186, 100),
-          ),
+        onPressed: () {},
+        child: Icon(Icons.chat_bubble_outline),
+        backgroundColor: Color.fromRGBO(226, 212, 186, 100),
+      ),
     );
   }
 }
