4cf6608a591687947bb5f0c8172242e73377696e
423018 <138673555+423018@users.noreply.github.com>
2023-09-25 00:08:25 +0900

    Update Resetting.dart

--- lib/view/Resetting/Resetting.dart
+++ lib/view/Resetting/Resetting.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:helloworld/view/Login/Login.dart';
+
 
 class ResettingPage extends StatefulWidget {
   const ResettingPage({super.key});
@@ -10,6 +12,15 @@ class ResettingPage extends StatefulWidget {
 class _ResettingPageState extends State<ResettingPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
+
+  String userInput = "";
+  bool isInputValid = false;
+
+  bool checkInput(String input) {
+    final RegExp regex = RegExp(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$');
+    return regex.hasMatch(input);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +41,13 @@ class _ResettingPageState extends State<ResettingPage> {
               child: Container(
                 width: 300,
                 child: TextField(
+                  obscureText: true,
+                  onChanged: (text) {
+                    setState(() {
+                      userInput = text;
+                      isInputValid = checkInput(userInput);
+                    });
+                  },
                   controller: emailController,
                   decoration: InputDecoration(hintText: 'パスワード'),
                 ),
@@ -38,6 +56,13 @@ class _ResettingPageState extends State<ResettingPage> {
             Container(
               width: 300,
               child: TextField(
+                obscureText: true,
+                onChanged: (text) {
+                  setState(() {
+                    userInput = text;
+                    isInputValid = checkInput(userInput);
+                  });
+                },
                 controller: passController,
                 decoration: InputDecoration(hintText: 'パスワード（確認用）'),
               ),
@@ -49,7 +74,20 @@ class _ResettingPageState extends State<ResettingPage> {
               ]),
             ),
             SizedBox(height: 70),
-            ElevatedButton(onPressed: () {}, child: Text('完了'))
+            ElevatedButton(
+              onPressed: () {
+                if (isInputValid) {
+                  print("文字列は条件を満たしています$passController");
+                } else {
+                  print("文字列は条件を満たしていません");
+                }
+                Navigator.pushReplacement(
+                  context, 
+                  MaterialPageRoute(builder: (context) =>SignUpPage())
+                );
+
+              }, 
+            child: Text('完了'))
           ],
         ),
       ),
