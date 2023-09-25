4b61951531fdffcb21618cdcd2785b5f6e755edf
423839hayato <423839@m.mie-u.ac.jp>
2023-09-25 13:11:51 +0900

    change New_register

--- lib/view/New_register/New_register.dart
+++ lib/view/New_register/New_register.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../Login/Login.dart';
+
 class New_register extends StatefulWidget {
   @override
   _New_registerState createState() => _New_registerState();
@@ -8,6 +10,59 @@ class New_register extends StatefulWidget {
 class _New_registerState extends State<New_register> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
+
+  String userInput = "";
+  String userInput1 = "";
+  bool isInputValid = false;
+
+  // 正規表現を使用して条件をチェックする関数
+  bool checkInput(String input) {
+    // 英語大文字、英語小文字、数字をそれぞれ少なくとも1文字以上使用している合計8文字以上の文字列
+    final RegExp regex = RegExp(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$');
+    return regex.hasMatch(input);
+  }
+
+  // ポップアップを表示する関数
+  void showValidationPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("入力が無効です"),
+          content: Text("英語大文字、英語小文字、数字をそれぞれ少なくとも1文字以上使用している合計8文字以上の文字列が必要です。"),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showValidationPopup1() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("入力が無効です"),
+          content: Text("入力された内容が一致しているか確認してください"),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +83,12 @@ class _New_registerState extends State<New_register> {
               child: Container(
                 width: 300,
                 child: TextField(
+                  onChanged: (text) {
+                    setState(() {
+                      userInput = text;
+                      isInputValid = checkInput(userInput);
+                    });
+                  },
                   controller: passController,
                   decoration: InputDecoration(hintText: 'パスワード'),
                 ),
@@ -36,11 +97,32 @@ class _New_registerState extends State<New_register> {
             Container(
               width: 300,
               child: TextField(
+                onChanged: (text) {
+                  setState(() {
+                    userInput1 = text;
+                  });
+                },
                 decoration: InputDecoration(hintText: 'パスワード（確認用）'),
               ),
             ),
             SizedBox(height: 70),
-            ElevatedButton(onPressed: () {}, child: Text('完了'))
+            ElevatedButton(
+                onPressed: () {
+                  if (isInputValid) {
+                    if (userInput == userInput1) {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignUpPage()));
+                    } else {
+                      showValidationPopup1();
+                    }
+                  } else {
+                    print("文字列は条件を満たしていません");
+                    showValidationPopup();
+                  }
+                },
+                child: Text('完了'))
           ],
         ),
       ),
