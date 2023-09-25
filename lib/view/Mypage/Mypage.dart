b21a024fa904a550c733cb15ef3130e50255a5b3
ibuki4002 <you@example.com>
2023-09-25 18:14:39 +0900

    change mypage to Mypage

--- lib/view/Mypage/Mypage.dart
+++ lib/view/Mypage/Mypage.dart
@@ -3,36 +3,35 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_fortune/model/account.dart';
 
-class mypage extends StatefulWidget {
-  const mypage({super.key});
+class Mypage extends StatefulWidget {
+  const Mypage({super.key});
 
   @override
-  State<mypage> createState() => _mypageState();
+  State<Mypage> createState() => _MypageState();
 }
 
-class _mypageState extends State<mypage> {
-  
+class _MypageState extends State<Mypage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Color.fromRGBO(226, 212, 186, 100),
-          elevation: 0,
-          iconTheme: IconThemeData(color: Colors.white),
-          title: Text(
-            'マイページ',
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
+        backgroundColor: Color.fromRGBO(226, 212, 186, 100),
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          'マイページ',
+          style: TextStyle(color: Colors.white),
         ),
+        centerTitle: true,
+      ),
       body: SafeArea(
-        child:Column(
-          children: [
-            Container(
-              height: 200,
-
-            )
-          ],) ),
+          child: Column(
+        children: [
+          Container(
+            height: 200,
+          )
+        ],
+      )),
     );
   }
 }
