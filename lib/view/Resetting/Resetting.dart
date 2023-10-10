60326a5187566aea95209ba12e43c7ea802a3b33
谷口 <423018@m.mie-u.ac.jp>
2023-10-10 19:46:38 +0900

    ResettingPage→Resetting

--- lib/view/Resetting/Resetting.dart
+++ lib/view/Resetting/Resetting.dart
@@ -2,24 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_fortune/view/Login/Login.dart';
 
 
-class ResettingPage extends StatefulWidget {
-  const ResettingPage({super.key});
+class Resetting extends StatefulWidget {
+  const Resetting({super.key});
 
   @override
-  State<ResettingPage> createState() => _ResettingPageState();
+  State<Resetting> createState() => _ResettingState();
 }
 
-class _ResettingPageState extends State<ResettingPage> {
+class _ResettingState extends State<Resetting> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
-
-  String userInput = "";
-  bool isInputValid = false;
-
-  bool checkInput(String input) {
-    final RegExp regex = RegExp(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$');
-    return regex.hasMatch(input);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +22,7 @@ class _ResettingPageState extends State<ResettingPage> {
         child: Column(
           children: [
             SizedBox(
-              height: 58,
+              height: 50,
             ),
             Text(
               '再設定ページ',
@@ -41,53 +33,21 @@ class _ResettingPageState extends State<ResettingPage> {
               child: Container(
                 width: 300,
                 child: TextField(
-                  obscureText: true,
-                  onChanged: (text) {
-                    setState(() {
-                      userInput = text;
-                      isInputValid = checkInput(userInput);
-                    });
-                  },
                   controller: emailController,
-                  decoration: InputDecoration(hintText: 'パスワード'),
+                  decoration: InputDecoration(hintText: 'メールアドレス'),
                 ),
               ),
-            ),
-            Container(
-              width: 300,
-              child: TextField(
-                obscureText: true,
-                onChanged: (text) {
-                  setState(() {
-                    userInput = text;
-                    isInputValid = checkInput(userInput);
-                  });
-                },
-                controller: passController,
-                decoration: InputDecoration(hintText: 'パスワード（確認用）'),
-              ),
-            ),
-            SizedBox(height: 10),
-            RichText(
-              text: TextSpan(style: TextStyle(color: Colors.black), children: [
-                TextSpan(text: '新しいパスワードを設定してください。'),
-              ]),
             ),
             SizedBox(height: 70),
             ElevatedButton(
               onPressed: () {
-                if (isInputValid) {
-                  print("文字列は条件を満たしています$passController");
-                } else {
-                  print("文字列は条件を満たしていません");
-                }
-                Navigator.pushReplacement(
-                  context, 
-                  MaterialPageRoute(builder: (context) =>LogIn())
-                );
+                //Navigator.pushReplacement(
+                  //context, 
+                  //MaterialPageRoute(builder: (context) => ())
+                //);
 
               }, 
-            child: Text('完了'))
+            child: Text('メールを送信'))
           ],
         ),
       ),
