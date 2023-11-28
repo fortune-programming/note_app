bbf0b2908900caf1e3b80adcdf5bcc950c3a885d
谷口 <423018@m.mie-u.ac.jp>
2023-11-28 16:55:04 +0900

    create Resetting_check

---
+++ lib/view/Resetting_check/Resetting_check.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Resetting_check extends StatefulWidget {
+  const Resetting_check({super.key});
+
+  @override
+  State<Resetting_check> createState() => _Resetting_checkState();
+}
+
+class _Resetting_checkState extends State<Resetting_check> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        title: Text('メールを送信しました', style: TextStyle(color: Color.fromRGBO(0, 0, 0, 1), fontSize: 30),),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CheckboxListTile(
+            controlAffinity: ListTileControlAffinity.leading,
+            title: Text('メールのURLをクリックしましたか', style: TextStyle(fontSize: 20)),
+              value: isChecked,
+              onChanged: (value){
+                setState(() { isChecked = !isChecked;});
+              },
+          ),
+          TextButton(
+            onPressed: () {
+
+            },
+            child: Text(isChecked ? 'ログインページに戻る':''),
+          )
+          ]
+      ),
+    ),
+    );
+  }
+}
