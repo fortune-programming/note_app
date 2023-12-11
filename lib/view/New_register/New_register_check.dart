a54bed75466da79ad16f4fa21ae5cde5aeb21a44
423839hayato <423839@m.mie-u.ac.jp>
2023-12-11 13:06:34 +0900

    copy resetting_check and creat checkbox

--- lib/view/New_register/New_register_check.dart
+++ lib/view/New_register/New_register_check.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import '../Login/Login.dart';
 
 class New_register_check extends StatefulWidget {
   @override
@@ -13,52 +12,40 @@ class _New_register_checkState extends State<New_register_check> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'メールを送信しました',
-            style: TextStyle(fontSize: 20.0),
-          ),
-          SizedBox(height: 20.0),
-          Container(
-            width: 300.0,
-            padding: EdgeInsets.all(20.0),
-            decoration: BoxDecoration(
-              color: Colors.red.withOpacity(0.3),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Row(
-              children: [
-                Checkbox(
-                  value: isChecked,
-                  onChanged: (bool? newValue) {
-                    if (newValue != null) {
-                      setState(() {
-                        isChecked = newValue;
-                      });
-                    }
-                  },
-                ),
-                ElevatedButton(
-                  onPressed: isChecked
-                      ? () {
-                          // ボタンが押せる処理
-                        }
-                      : null, // チェックがない場合はボタンを無効にする
-                  child: Text('次へ進む'),
-                ),
-                SizedBox(width: 10.0),
-                Text(
-                  'メールのURLをクリックした',
-                  style: TextStyle(fontSize: 16.0),
-                ),
-              ],
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'メールを送信しました',
+          style: TextStyle(color: Color.fromRGBO(0, 0, 0, 1), fontSize: 30),
+        ),
       ),
-    ));
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CheckboxListTile(
+                controlAffinity: ListTileControlAffinity.leading,
+                title:
+                    Text('メールのURLをクリックしましたか', style: TextStyle(fontSize: 20)),
+                value: isChecked,
+                onChanged: (value) {
+                  setState(() {
+                    isChecked = !isChecked;
+                  });
+                },
+              ),
+              ElevatedButton(
+                onPressed: isChecked
+                    ? () {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) => LogIn()));
+                      }
+                    : null, // チェックがない場合はボタンを無効にする
+                child: Text('ログインページに戻る'),
+              ),
+            ]),
+      ),
+    );
   }
 }
