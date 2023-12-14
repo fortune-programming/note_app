58fe497e3ac9c642c058d1d5571d7a5b3d509d86
423839hayato <423839@m.mie-u.ac.jp>
2023-12-14 14:32:39 +0900

    create checkbox

--- lib/view/Resetting_check/Resetting_check.dart
+++ lib/view/Resetting_check/Resetting_check.dart
@@ -17,31 +17,37 @@ class _Resetting_checkState extends State<Resetting_check> {
       appBar: AppBar(
         elevation: 0.0,
         backgroundColor: Colors.transparent,
-        title: Text('メールを送信しました', style: TextStyle(color: Color.fromRGBO(0, 0, 0, 1), fontSize: 30),),
+        title: Text(
+          'メールを送信しました',
+          style: TextStyle(color: Color.fromRGBO(0, 0, 0, 1), fontSize: 30),
+        ),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CheckboxListTile(
-            controlAffinity: ListTileControlAffinity.leading,
-            title: Text('メールのURLをクリックしましたか', style: TextStyle(fontSize: 20)),
-              value: isChecked,
-              onChanged: (value){
-                setState(() { isChecked = !isChecked;});
-              },
-          ),
-          TextButton(
-            onPressed: () {
-               Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => LogIn())
-                  );
-            },
-            child: Text(isChecked ? 'ログインページに戻る':''),
-          )
-        ]
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CheckboxListTile(
+                controlAffinity: ListTileControlAffinity.leading,
+                title:
+                    Text('メールのURLをクリックしましたか', style: TextStyle(fontSize: 20)),
+                value: isChecked,
+                onChanged: (value) {
+                  setState(() {
+                    isChecked = !isChecked;
+                  });
+                },
+              ),
+              ElevatedButton(
+                onPressed: isChecked
+                    ? () {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) => LogIn()));
+                      }
+                    : null, // チェックがない場合はボタンを無効にする
+                child: Text('ログインページに戻る'),
+              ),
+            ]),
       ),
-     ),
     );
   }
 }
