dcc0f2a043449ceef75f601ee97adca519d5a064
423839hayato <423839@m.mie-u.ac.jp>
2023-12-18 23:32:32 +0900

    added sendPasswordResetEmail system

--- lib/view/Resetting/Resetting.dart
+++ lib/view/Resetting/Resetting.dart
@@ -1,7 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune/view/Login/Login.dart';
 import 'package:flutter_fortune/view/Resetting_check/Resetting_check.dart';
-
 
 class Resetting extends StatefulWidget {
   const Resetting({super.key});
@@ -17,60 +17,68 @@ class _ResettingState extends State<Resetting> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Color(0xFFE2D4BA),
-        title: Center(
-          child: Text(
-            '再設定ページ',
-            style: TextStyle(
-              color: Colors.white,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Color(0xFFE2D4BA),
+          title: Center(
+            child: Text(
+              '再設定ページ',
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
           ),
         ),
-      ),
         body: SafeArea(
-      child: Container(
-        width: double.infinity,
-        child: Column(
-          children: [
-            //SizedBox(
-              //height: 50,
-            //),
-            //Text(
-              //'再設定ページ',
-              //style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            //),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: Container(
-                width: 300,
-                child: TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    hintText: 'メールアドレス',
-                    suffixIcon: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        '@m.mie-u.ac.jp',
-                        style: TextStyle(color: Colors.black),
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              children: [
+                //SizedBox(
+                //height: 50,
+                //),
+                //Text(
+                //'再設定ページ',
+                //style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                //),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Container(
+                    width: 300,
+                    child: TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        hintText: 'メールアドレス',
+                        suffixIcon: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Text(
+                            '@m.mie-u.ac.jp',
+                            style: TextStyle(color: Colors.black),
+                          ),
                         ),
-                        ),
-                         ),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                SizedBox(height: 70),
+                OutlinedButton(
+                    onPressed: () async {
+                      try {
+                        await FirebaseAuth.instance.sendPasswordResetEmail(
+                            email: emailController.text);
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Resetting_check()));
+                        print("パスワードリセット用のメールを送信しました");
+                      } catch (e) {
+                        print(e);
+                      }
+                    },
+                    child: Text('メールを送信'))
+              ],
             ),
-            SizedBox(height: 70),
-            OutlinedButton(
-              onPressed: () {
-                Navigator.pushReplacement(context, 
-                  MaterialPageRoute(builder: (context) => Resetting_check())
-                );
-              }, 
-            child: Text('メールを送信'))
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 }
