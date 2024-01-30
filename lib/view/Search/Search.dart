4e3c9f484a3c9c5b6241b1a8507b58d2a0adc5a2
423839hayato <423839@m.mie-u.ac.jp>
2024-01-30 23:36:53 +0900

    add userSearch prediction

--- lib/view/Search/Search.dart
+++ lib/view/Search/Search.dart
@@ -15,8 +15,45 @@ class Search extends StatefulWidget implements PreferredSizeWidget {
 
 class _SearchState extends State<Search> {
   TextEditingController searchController = TextEditingController();
-
   final _controller = TextEditingController();
+  UserFirestore _userFirestore = UserFirestore();
+
+  List<Map<String, dynamic>> _searchResults = [];
+
+  _searchUsers(String searchQuery) async {
+    if (searchQuery.isNotEmpty) {
+      List<Map<String, dynamic>> results =
+          await _userFirestore.searchService.searchUsersByNumber(searchQuery);
+      setState(() {
+        // Firestoreから取得した結果をクライアントサイドでフィルタリング
+        _searchResults = results
+            .where((user) => user['number'].startsWith(searchQuery))
+            .toList();
+      });
+    } else {
+      setState(() {
+        _searchResults.clear();
+      });
+    }
+  }
+
+  _onUserSearchSubmitted(String text) async {
+    print("検索実行");
+    var _result = await UserFirestore.getUserByNumber(text);
+    if (_result == true) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Display()));
+    } else {
+      print("検索エラー");
+    }
+  }
+
+  _onUserTapped(Map<String, dynamic> user) {
+    // ユーザーがクリックされたときの処理を追加
+    print('User tapped: $user');
+    // ここで検索を実行する処理を呼び出す
+    _onUserSearchSubmitted(user['number']);
+  }
 
   void _submission(text) {
     setState(() {
@@ -57,19 +94,43 @@ class _SearchState extends State<Search> {
                       focusedBorder: InputBorder.none,
                       isDense: true,
                     ),
+                    onChanged: (value) {
+                      _searchUsers(value);
+                    },
                     onSubmitted: (text) async {
-                      print("検索実行");
-                      var _result = await UserFirestore.getUserByNumber(text);
-                      if (_result == true) {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => Display()));
-                      } else {
-                        print("検索エラー");
-                      }
+                      _onUserSearchSubmitted(text);
                     }),
               ),
             ),
           ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SizedBox(height: 16),
+            Expanded(
+              child: _searchResults.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: _searchResults.length,
+                      itemBuilder: (context, index) {
+                        Map<String, dynamic> user = _searchResults[index];
+                        return ListTile(
+                          title: Text(user['name']),
+                          subtitle: Text('@${user['number']}'),
+
+                          onTap: () => _onUserTapped(user),
+                          // 他のユーザー情報も表示できるように調整
+                          // 例: subtitle: Text(user['number']),
+                        );
+                      },
+                    )
+                  : Center(
+                      child: Text('アカウントが見つかりません'),
+                    ),
+            ),
+          ],
         ),
       ),
     );
