76740f2b02940ee451268ffd3f4a8b93ab9398fe
{高瀬} <{423553@m.mie-u.ac.jp}>
2024-03-07 23:59:34 +0900

    create note_page

---
+++ lib/view/note/note_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Notepage extends StatefulWidget {
+  const Notepage({super.key});
+
+  @override
+  State<Notepage> createState() => _NotepageState();
+}
+
+class _NotepageState extends State<Notepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/view/note/note_page_not_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Notepage_notbutton extends StatefulWidget {
+  const Notepage_notbutton({super.key});
+
+  @override
+  State<Notepage_notbutton> createState() => _Notepage_notbuttonState();
+}
+
+class _Notepage_notbuttonState extends State<Notepage_notbutton> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
